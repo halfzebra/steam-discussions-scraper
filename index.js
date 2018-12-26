deb2f7c4356803a0823865d2ce41d848a70bf47d
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-12-26 23:36:29 +0100

    Plan the next steps

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 
-// Response:
-//
-// {
-//     success: number;
-//     start: number;
-//     total_count: number;
-//     topics_html: string;
-// }
-
 // 1. Request topics with offset 0 and count 100
 // 2. Check if there are more topics left
 // 3. Fetch the next 100 topics, and if there are then go to 2.
-// 4. Start fetching pages with comments
+// 4. Start fetching comments for topics if no more topics left
 // 5. Fetch the page starting from 0 with the first 50 comments
 // 6. Check if it was the last page, otherwise go to 5.
 // 7. Start fetching the next topic.
+
+// Response:
+//
+// {
+//     total: number;
+//     offset: number;
+//     count: number;
+//     topics: []{ topicId: number; title: string; }
+// }
 
 function requestTopics(appId, subForumPath, pageNumber) {
   const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/?fp=' + pageNumber;
@@ -51,6 +51,20 @@ function requestTopics(appId, subForumPath, pageNumber) {
       })
   )
 }
+
+// Response:
+//
+// {
+//     op: {
+//       // some info about the op post
+//     };
+//     offset: number;
+//     count: number;
+//     comments: []{
+//      author: // some info about the author
+//      text: string;
+//     };
+// }
 
 function requestTopicComments(appId, topicId, subForumPath, pageNumber) {
   const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/' + topicId + '/?ctp=' + pageNumber;
