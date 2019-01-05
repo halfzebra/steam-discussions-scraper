ef0dd0666d8a341ff513e146dab13505866668eb
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-01-05 22:14:58 +0100

    Updated all the helpers for scraping the discusstions

--- index.js
+++ index.js
@@ -20,43 +20,45 @@ const path = require('path');
 //     topics: []{ topicId: number; title: string; }
 // }
 
-function requestTopics(appId, subForumPath, pageNumber) {
+function requestSubForumPage(appId, subForumPath, pageNumber) {
+	const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/?fp=' + pageNumber;
+	const headers = {
+		Cookie:
+			'rgDiscussionPrefs=' + encodeURIComponent(JSON.stringify({ cTopicRepliesPerPage: 50, cTopicsPerPage: 50 }))
+	};
 
-  const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/?fp=' + pageNumber;
-  const headers = {
-    Cookie: 'rgDiscussionPrefs=' + encodeURIComponent(JSON.stringify({ cTopicRepliesPerPage: 50, cTopicsPerPage: 50 }))
-  }
-  return (
-    Promise.resolve({ data: fs.readFileSync(path.resolve(__dirname, './fixture/topics.html')) })
+	return axios.get(url, {
+		headers
+	});
+}
 
-      // axios.get(url, {
-      //   headers
-      // })
-      .then(({ data }) => {
-        const offset = (pageNumber - 1) * 50
-        const topics = []
-        const $ = cheerio.load(data);
+async function scrapeTopicsPage(appId, subForumPath, pageNumber) {
+	try {
+		const { data } = await requestSubForumPage(appId, subForumPath, pageNumber);
+		const offset = (pageNumber - 1) * 50;
+		const topics = [];
+		const $ = cheerio.load(data);
 
-        $('.forum_topics_container .forum_topic').each((_, topicHtml) => {
-          topics.push({
-            topicId: $(topicHtml).attr('data-gidforumtopic'),
-            title: $(topicHtml).find('.forum_topic_name').text().trim()
-          })
-        })
+		$('.forum_topics_container .forum_topic').each((_, topicHtml) => {
+			topics.push({
+				id: $(topicHtml).attr('data-gidforumtopic'),
+				title: $(topicHtml).find('.forum_topic_name').text().trim()
+			});
+		});
 
+		const count = topics.length;
+		const totalContainer = $('.forum_paging_header span').get(3);
+		const total = $(totalContainer).text();
 
-        const count = topics.length;
-        const totalContainer = $('.forum_paging_header span').get(3);
-        const total = $(totalContainer).text();
-
-        return {
-          total,
-          offset,
-          count,
-          topics
-        }
-      })
-  )
+		return {
+			total,
+			offset,
+			count,
+			topics
+		};
+	} catch (error) {
+		throw error;
+	}
 }
 
 // Response:
@@ -78,95 +80,112 @@ function requestTopics(appId, subForumPath, pageNumber) {
 //     };
 // }
 
-function requestTopicComments(appId, topicId, subForumPath, pageNumber) {
-  const offset = (pageNumber - 1) * 50
-  const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/' + topicId + '/?ctp=' + pageNumber;
-  const headers = {
-    Cookie: 'rgDiscussionPrefs=' + encodeURIComponent(JSON.stringify({ cTopicRepliesPerPage: 50, cTopicsPerPage: 50 }))
-  }
-  return (
-    axios.get(url, {
-      headers
-    })
-    // Promise.resolve({ data: fs.readFileSync(path.resolve(__dirname, './fixture/comments.html'), 'utf8') })
-      .then(({ data }) => {
-        const $ = cheerio.load(data)
+function scrapeTopicOp($) {
+	const $op = $('.forum_op');
+	const timestamp = parseInt($op.find('.date').attr('data-timestamp'));
+	const date = new Date(timestamp * 1000);
+	const title = $op.find('.topic').text().trim();
+	const html = $op.find('.content').html();
+	const author = $op.find('.forum_op_author').attr('href');
 
-        const comments = [];
-
-        $('.commentthread_comment').each((index, commentHtml) => {
-          const $comment = $(commentHtml);
-          const timestamp = $comment.find('.commentthread_comment_timestamp').attr('data-timestamp');
-          comments.push({
-            author: $comment.find('.commentthread_author_link').attr('href'),
-            date: new Date(timestamp * 1000),
-            html: $comment.find('.commentthread_comment_text').html(),
-            hasGame: $comment.find('.commentthread_comment_author > img').length === 1
-          })
-        });
-
-        const count = comments.length;
-
-        // total
-        const totalContainer = $($('.forum_paging_summary').get(0)).find('span').get(2);
-        const total = parseInt($(totalContainer).text());
-
-        // op
-        const $op = $('.forum_op')
-        const timestamp = parseInt($op.find('.date').attr('data-timestamp'));
-        const date = new Date(timestamp * 1000);
-        const title = $op.find('.topic').text().trim();
-        const html = $op.find('.content').html();
-        const author = $op.find('.forum_op_author').attr('href')
-
-        const op = {
-          author,
-          title,
-          date,
-          html
-        }
-
-        return {
-          op,
-          total,
-          offset,
-          count,
-          comments
-        }
-      })
-  )
+	return {
+		author,
+		title,
+		date,
+		html
+	};
 }
 
-async function main() {
-  try {
-    // const { topics } = await requestTopics(711660, '/discussions/0', 1)
-    // console.log(topics);
-    // const { topicId } = topics[0];
+function scrapeTopicComments($) {
+	const comments = [];
 
-    let page = 1
-    let done = false
+	$('.commentthread_comment').each((index, commentHtml) => {
+		const $comment = $(commentHtml);
+		const timestamp = $comment.find('.commentthread_comment_timestamp').attr('data-timestamp');
+		comments.push({
+			author: $comment.find('.commentthread_author_link').attr('href'),
+			date: new Date(timestamp * 1000),
+			html: $comment.find('.commentthread_comment_text').html().trim(),
+			hasGame: $comment.find('.commentthread_comment_author > img').length === 1
+		});
+	});
 
-    let allComments = []
-
-    while (!done) {
-      const { total, comments } = await requestTopicComments(440, '1744480967012786259', '/discussions/0', page)
-      allComments.push(comments);
-
-      if (total > page * 50) {
-        page++
-      } else {
-        done = true;
-      }
-    }
-
-    fs.writeFileSync(path.resolve(__dirname, Date.now() + '.json'), JSON.stringify(allComments, null, 4))
-
-    console.log(allComments.length)
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	return comments;
 }
 
+function requestCommentsPage(appId, topicId, subForumPath, pageNumber) {
+	const url = 'https://steamcommunity.com/app/' + appId + subForumPath + '/' + topicId + '/?ctp=' + pageNumber;
+	const headers = {
+		Cookie:
+			'rgDiscussionPrefs=' + encodeURIComponent(JSON.stringify({ cTopicRepliesPerPage: 50, cTopicsPerPage: 50 }))
+	};
 
-main();
+	return axios.get(url, {
+		headers
+	});
+}
+
+async function scrapeCommentsPage(appId, topicId, subForumPath, pageNumber) {
+	try {
+		const offset = (pageNumber - 1) * 50;
+		const { data } = await requestCommentsPage(appId, topicId, subForumPath, pageNumber);
+
+		const $ = cheerio.load(data);
+		const comments = scrapeTopicComments($);
+		const count = comments.length;
+
+		// totalComments
+		const totalContainer = $($('.forum_paging_summary').get(0)).find('span').get(2);
+		const total = parseInt($(totalContainer).text());
+
+		return {
+			total,
+			offset,
+			count,
+			comments
+		};
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function scrapeTopicsInSubForum(appId, subForumPath) {
+	let page = 1;
+	let done = false;
+
+	let allTopics = [];
+
+	while (!done) {
+		const { total, topics } = await scrapeTopicsPage(appId, subForumPath, page);
+		allTopics.push(topics);
+
+		if (total > page * 50) {
+			page++;
+		} else {
+			done = true;
+		}
+	}
+
+	return allTopics;
+}
+
+async function scrapeCommentsInTopic(appId, subForumPath, topicId) {
+	let page = 1;
+	let done = false;
+	let allComments = [];
+
+	while (!done) {
+		const { total, comments } = await scrapeCommentsPage(appId, topicId, subForumPath, page);
+		allComments.push(comments);
+
+		if (total > page * 50) {
+			page++;
+		} else {
+			done = true;
+		}
+	}
+
+	return allComments;
+}
+
+module.exports = { requestSubForumPage, requestCommentsPage, scrapeTopicsInSubForum, scrapeCommentsInTopic };
